0aa79719d8ef458b0d597b9ded72d8a60917bb59
camilo <cacb2908@gmail.com>
2026-03-03 08:37:20 -0500

    nuevo commit

--- devs/events-generator.py
+++ devs/events-generator.py
@@ -1,1 +1,47 @@
-n=(input("hola: "))
+import random
+
+
+
+random_event = random.randint(1, 10)
+print(random_event)
+
+if random_event == 1:
+    print=("ola de calor extrema")
+    
+if random_event == 2:
+    print=("falla en el sistema de refrigeracion")
+
+if random_event == 3:
+    print=("sobrecarga electrica")
+
+if random_event == 4:
+    print=("incendio en sala tecnica")
+    
+if random_event == 5:
+    print=("inundaciones por tuberia rota")
+
+if random_event == 6:
+    print=("ciberataque")
+
+if random_event == 7:
+    print=("colapso de la red")
+    
+if random_event == 8:
+    print=("virus interno")
+
+if random_event == 9:
+    print=("terremoto")
+
+if random_event == 10:
+    print=("recorte de personal")
+    
+
+
+   
+       
+
+
+ 
+
+
+
