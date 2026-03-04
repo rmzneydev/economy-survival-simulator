45cfc31f42ddb5ea513eb7bde45d811e3854f068
cristian <cristian@gmail.com>
2026-03-04 09:45:34 -0500

    se coloco  en funcio

--- devs/defeat_validator.py
+++ devs/defeat_validator.py
@@ -1,26 +1,27 @@
-servidores = 0
-refrigeracion= 0
-clientes = 0
-temperatura = 80
+def GameOver(servidores, refrigeracion, clientes, temperatura):
+    # Si los servidores llegan a 0
+    if servidores <= 0:
+        print(" Todos los servidores dejaron de funcionar")
+        print("Perdiste")
+        return False
 
-# Si los servidores llegan a 0
-if servidores <= 0:
-    print(" Todos los servidores dejaron de funcionar")
-    derrota = True
+    # Si la refrigeración llega a 0
+    if refrigeracion <= 0:
+        print(" El sistema de refrigeración colapsó")
+        print("Perdiste")
+        return False
 
-# Si la refrigeración llega a 0
-if refrigeracion <= 0:
-    print(" El sistema de refrigeración colapsó")
-    derrota = True
+    # Si los clientes llegan a 0
+    if clientes <= 0:
+        print(" Perdiste todos los clientes")
+        print("Perdiste")
+        return False
 
-# Si los clientes llegan a 0
-if clientes <= 0:
-    print(" Perdiste todos los clientes")
-    derrota = True
+    # Evento crítico de temperatura
+    if temperatura > 80:
+        print("Temperatura mayor a 80°C")
+        print("SERVIDORES QUEMADOS")
+        print("Perdiste")
+        return False
 
-# Evento crítico de temperatura
-if temperatura > 80:
-    print("Temperatura mayor a 80°C")
-    print("SERVIDORES QUEMADOS")
-    derrota = True
-    
+GameOver(7, 5, 6, 60)
