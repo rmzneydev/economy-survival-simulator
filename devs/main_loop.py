1abed502a453de1614540efff73338203f7d078d
Andres Elles <andreselles1107@gmail.com>
2026-03-05 09:36:07 -0500

    main_loop v3

--- devs/main_loop.py
+++ devs/main_loop.py
@@ -1,8 +1,11 @@
+import random
 from data_entry import setName, setDifficult, getResources
 
 name = setName()
 difficult = setDifficult()
 resource = getResources(difficult) 
+
+
 
 
 def MainLoop(): 
@@ -16,6 +19,13 @@ def MainLoop():
     recursos = 15
     lvl = 2
 
+    # lista de días
+    days = ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Viernes", "Sábado", "Domingo"]
+
+    # Escoger un día aleatorio
+
+    current_day = random.randint(0,7)
+
     #Bucle while para repetir los 10 días del juego
     while Day <= 10 or GameOver == True:
         
@@ -26,6 +36,9 @@ def MainLoop():
         
 
         Day += 1
+        current_day += 1
+        if current_day == 7:
+            current_day = 0
 
 MainLoop()
 
