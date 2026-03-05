6a28c8a0b5e7b6b35beb2b705216e7b547a88985
Sharon Meriño <smmerino@outlook.com>
2026-03-04 21:55:00 -0500

    changes in data-entry.py added validation for invalid inputs

--- devs/data-entry.py
+++ devs/data-entry.py
@@ -10,11 +10,24 @@ def data_entry():
         
 
     print("\nSelect the difficulty level: ")
-    print("1. easy")
-    print("2. medium")
-    print("3. hard")
-        
-    difficulty = input("option (1-3):  \n")
+    print("1. Easy")
+    print("2. Medium")
+    print("3. Hard")
+
+    cond = False
+    while not cond:
+        try:
+            difficulty = int(input("Option (1-3):  \n"))
+            if difficulty ==1:
+                cond=True
+            elif difficulty ==2:
+                cond=True
+            elif difficulty ==3:
+                cond=True
+            else:
+                print("Invalid option. Please select a number for one to three")    
+        except:
+            print("Invalid option. Please select a number for one to three")
 
     server_capacity = 0
     temperature = 0    
@@ -22,40 +35,30 @@ def data_entry():
         
         #asignacion de recursos segun dificultad
 
-    if difficulty == "1":
+    if difficulty == 1:
         server_capacity = 100
         temperature = 30    
         clients = 100  
         
         print(f"level: easy\n, server_capacity: {server_capacity}\n, temperature: {temperature}\n, clients: {clients}")
     
-    elif difficulty == "2":
+    elif difficulty == 2:
         server_capacity = 50
         temperature = 50    
         clients = 50  
         
-        print(f"level: medium\n, server_capacity: {server_capacity}\n, temperature: {temperature}\n, clients: {clients}")
+        print(f"level: Medium\n, server_capacity: {server_capacity}\n, temperature: {temperature}\n, clients: {clients}")
         
-    elif difficulty == "3":
+    elif difficulty == 3:
         server_capacity = 20
         temperature = 70    
         clients = 20  
         
-        print(f"level: hard\n, server_capacity: {server_capacity}\n, temperature: {temperature}\n, clients: {clients}")    
-    
-        
-        
-    else:
-        server_capacity = 50
-        temperature = 50    
-        clients = 50 
-        difficulty = "2"
-        print("Invalid option. Medium difficulty will be assigned by default. ")
-        print(f"level: medium\n, server_capacity: {server_capacity}\n, temperature: {temperature}\n, clients: {clients}")
+        print(f"level: Hard\n, server_capacity: {server_capacity}\n, temperature: {temperature}\n, clients: {clients}")    
 
     resources = [server_capacity, temperature, clients]
     return resources
-
+data_entry()
 
 
 
