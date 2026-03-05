94d555e8de1a772d7f00df2f5451ce66586cae18
Andres Elles <andreselles1107@gmail.com>
2026-03-05 08:25:23 -0500

    main_loop

--- devs/main_loop.py
+++ devs/main_loop.py
@@ -1,6 +1,6 @@
 def MainLoop(): 
 
-    # Creación de variables
+    # Creación de variables de prueba
 
     Day = 1
     GameOver = 0
@@ -10,8 +10,8 @@ def MainLoop():
     lvl = 2
 
     #Bucle while para repetir los 10 días del juego
-    while Day <= 10:
-
+    while Day <= 10 or GameOver == True:
+        
         #Mostrar en consola el día que está, el evento y el consumo del recurso
         print("Day ", Day)
         print(Problem)
