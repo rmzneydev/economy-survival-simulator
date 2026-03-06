6d122c039941565dc40acbb3f93484090f6c34a6
Andres Elles <andreselles1107@gmail.com>
2026-03-06 07:20:45 -0500

    Fix Week Days

--- devs/main_loop.py
+++ devs/main_loop.py
@@ -1,9 +1,9 @@
 import random
-from data_entry import setName, setDifficult, getResources
+# from data_entry import setName, setDifficult, getResources
 
-name = setName()
-difficult = setDifficult()
-resource = getResources(difficult) 
+# name = setName()
+# difficult = setDifficult()
+# resource = getResources(difficult) 
 
 
 
@@ -14,31 +14,30 @@ def MainLoop():
 
     Day = 1
     GameOver = 0
-    Problem = "Subió la temperatura 5°"
-    consumption = 4
-    recursos = 15
-    lvl = 2
+    # Problem = "Subió la temperatura 5°"
+    # consumption = 4
+    # recursos = 15
+    # lvl = 2
 
     # lista de días
-    days = ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Viernes", "Sábado", "Domingo"]
+    days = ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"]
 
     # Escoger un día aleatorio
 
-    current_day = random.randint(0,7)
+    current_day = random.randint(0,6)
 
     #Bucle while para repetir los 10 días del juego
     while Day <= 10 or GameOver == True:
         
         #Mostrar en consola el día que está, el evento y el consumo del recurso
-        print("Day ", Day)
-        print(Problem)
-        print("has consumido", consumption, "recursos")
+        print("Day", Day, ": ", days[current_day])
         
 
         Day += 1
-        current_day += 1
-        if current_day == 7:
+        if current_day == 6:
             current_day = 0
+        else:
+            current_day +=1
 
 MainLoop()
 
