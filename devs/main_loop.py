a0fc1c5e0169478be2d4c9ca732287457b90679b
Andres Elles <andreselles1107@gmail.com>
2026-03-06 09:38:24 -0500

    Change GameOver item

--- devs/main_loop.py
+++ devs/main_loop.py
@@ -13,11 +13,8 @@ def MainLoop():
     resource = getResources(difficult) 
 
     Day = 1
-    GameOver = 0
-    # Problem = "Subió la temperatura 5°"
-    # consumption = 4
-    # recursos = 15
-    # lvl = 2
+    GameOver = False
+
 
     # lista de días
     days = ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"]
@@ -27,7 +24,7 @@ def MainLoop():
     current_day = random.randint(0,6)
 
     #Bucle while para repetir los 10 días del juego
-    while Day <= 10 or GameOver == True:
+    while Day <= 10 and GameOver == False:
 
         #Mostrar en consola el día que está, el evento y el consumo del recurso
         print("Day", Day, ": ", days[current_day])
@@ -35,7 +32,6 @@ def MainLoop():
         print("Events: ", event[1])
         number_of_event = event[0]
       
-        
         consumption = calculate_consumption(number_of_event, difficult, resource, current_day)
         temperature_before = increase_temperature(number_of_event, resource[3])
         consumption.append(temperature_before)
@@ -48,8 +44,8 @@ def MainLoop():
 
 
 
-              
-                     
+
+
         Day += 1
         if current_day == 6:
             current_day = 0
