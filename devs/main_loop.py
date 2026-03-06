08788f1029726056d2ae00b4ec199cfd540bf92b
Andres Elles <andreselles1107@gmail.com>
2026-03-06 10:20:32 -0500

    added defeat funtions

--- devs/main_loop.py
+++ devs/main_loop.py
@@ -2,6 +2,7 @@ import random
 from data_entry import setName, setDifficult, getResources
 from events_generator import get_events
 from consumption import calculate_consumption, increase_temperature
+from defeat_validator import GameOver
 
 
 
@@ -13,7 +14,7 @@ def MainLoop():
     resource = getResources(difficult) 
 
     Day = 1
-    GameOver = False
+    keep_playing = False
 
 
     # lista de días
@@ -24,7 +25,7 @@ def MainLoop():
     current_day = random.randint(0,6)
 
     #Bucle while para repetir los 10 días del juego
-    while Day <= 10 and GameOver == False:
+    while Day <= 10 and keep_playing == False:
 
         #Mostrar en consola el día que está, el evento y el consumo del recurso
         print("Day", Day, ": ", days[current_day])
@@ -36,6 +37,13 @@ def MainLoop():
         temperature_before = increase_temperature(number_of_event, resource[3])
         consumption.append(temperature_before)
         resource = consumption
+        servers = resource[0]
+        cooling = resource[1]
+        clients = resource[2]
+        temperature = resource[3]
+        print(resource)
+        
+        keep_playing = GameOver(servers, cooling, clients, temperature)
 
         input("Press any keys for continue to the next day: ")
 
