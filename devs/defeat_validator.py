eba9f0deb0b69540eb2c18cce09efd8e9f72f36d
jhosep-1997 <jhosepahum@gmail.com>
2026-03-08 18:11:36 -0500

    Refactor GameOver function to use show_game_over
    .The `SHOW_GAME OVER` function is imported from the `CONLOLA INTERFACE` file.
    .The critical event condition is placed before the others because if it is not checked beforehand, the game will continue.

--- devs/defeat_validator.py
+++ devs/defeat_validator.py
@@ -1,28 +1,25 @@
+from console_interface import show_game_over
+
 def GameOver(servers, cooling, clients, temperature):
-    # If servers reach 0
-    if servers <= 0:
-        print("All servers stopped working")
-        print("You lost")
+    # Evento Critico
+    if temperature > 80:
+        show_game_over("temperature")
+        return True
+    
+    # Servidores
+    elif servers <= 0:
+        show_game_over("All servers stopped working")
         return True
 
-    # elif cooling reaches 0
+    # Refrigeracion 
     elif cooling <= 0:
-        print("The cooling system collapsed")
-        print("You lost")
+        show_game_over("The cooling system collapsed")
         return True
 
-    # elif clients are greater than or equal to servers
+    # Clientes
     elif clients >= servers:
-        print("You lost all your clients")
-        print("You lost")
-        return True
-
-    # Critical temperature event
-    elif temperature > 80:
-        print("Temperature higher than 80°C")
-        print("SERVERS BURNED")
-        print("You lost")
-        return True
+        show_game_over("clients")
+        return True  
 
     else:
-        return False
+        return False 
